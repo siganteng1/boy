a917b10d93011014ff28079e7c536d1eba45805f
baperan <107087537+baperan@users.noreply.github.com>
2022-06-17 18:21:25 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -855,25 +855,39 @@ reply(`Kirim/Reply Gambar/Video Dengan Caption ${prefix + command}\n\nDurasi Sti
 }
 }
 break
-			case 'setppbot': case 'setpp': {
-                if (!m.key.fromMe && !isCreator) return reply(lang.ownerOnly())
-                if (!quoted) throw 'Reply Image'
-                if (!/image/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
-                if (/webp/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
-                let media = await alpha.downloadAndSaveMediaMessage(quoted)
-                await alpha.updateProfilePicture(alpha.user.id, { url: media }).catch((err) => fs.unlinkSync(media))
-                reply(lang.ok())
-                }
-                break
-			case 'setppgroup': case 'setppgrup': case 'setppgc': {
-                if (!m.isGroup) throw mess.group
-                if (!m.isGroup && !isBotAdmins && !isGroupAdmins) return reply(lang.adminOnly())
+			case 'setppbot': {
+                if (!isCreator) throw mess.owner
                 if (!quoted) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
                 if (!/image/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
                 if (/webp/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
-                let media = await alpha.downloadAndSaveMediaMessage(quoted)
-                await alpha.updateProfilePicture(m.chat, { url: media }).catch((err) => fs.unlinkSync(media))
-                reply(lang.ok())
+                var media = await tebece.downloadAndSaveMediaMessage(quoted)
+                try {
+                if (args[0] == "/full") {
+                const { generateProfilePicture } = require("./lib/myfunc")
+                var { img } = await generateProfilePicture(media)
+                await tebece.query({ tag: 'iq',  attrs: { to: botNumber, type:'set', xmlns: 'w:profile:picture'}, content: [{ tag: 'picture', attrs: { type: 'image' }, content: img }]})
+                } else { await tebece.updateProfilePicture(botNumber, { url: media }) }
+                m.reply(mess.success)
+                } catch { m.reply('Gagal Mengganti Photo Profile') }
+                }
+                break
+           case 'setppgroup': case 'setppgrup': case 'setppgc': {
+                if (!m.isGroup) throw mess.group
+                if (!isAdmins) throw mess.admin
+                if (!quoted) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+                if (!/image/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+                if (/webp/.test(mime)) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
+                let media = await tebece.downloadAndSaveMediaMessage(quoted)
+                try {
+                if (args[0] == "/full") {
+                const { generateProfilePicture } = require("./lib/myfunc")
+                var { img } = await generateProfilePicture(media)
+                await tebece.query({ tag: 'iq',  attrs: { to: m.chat, type:'set', xmlns: 'w:profile:picture'}, content: [{ tag: 'picture', attrs: { type: 'image' }, content: img }]})
+                } else { await tebece.updateProfilePicture(m.chat, { url: media }) }
+                m.reply(mess.success)
+                } catch { m.reply('Gagal Mengganti Photo Profile') }
+                }
+                break
                 }
                 break    
 			case 'toaud': case 'toaudio': {
